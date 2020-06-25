5ad5d76802958b6de7dd7b5fee37f942e10012ec
Warren Uhrich <hello@warren.codes>
2020-06-25 10:30:01 -0600

    Added datatype examples; added mathematical operators.

--- Program.cs
+++ Program.cs
@@ -6,37 +6,36 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            // Two slashes mark the beginning of a single-line comment.
-            // We are assigning a string value to a variable.
-            string myName = "Warren Uhrich";
-            /* We begin multi-line comments with a forward-slash and asterisk...
-            
-            Symbols between values or evaluations are considered "operators."
-            Below we are using the addition symbol to concatenate two strings.
-            (Concatentation means gluing two strings, or texts, together.)
+            // "=" is the assignment operator!
+            // String datatype is used for text.
+            string myString = "This is a string!";
+            // Integer datatype is used for whole numbers (no decimals.)
+            int myInt = 42;
+            // Double datatype is used for decimal point numbers.
+            double myDouble = 33.666;
+            // Boolean datatype is used for TRUE/FALSE values - think binary!
+            bool myBool = true;
 
-            ...and we end multi-line comments with an asterisk followed by a forward-slash */
-            Console.WriteLine("Hello, " + myName);
+            // Subtraction operator is "-" (minus.)
+            Console.WriteLine( myInt - 10 ); // 32
 
-            // User input...
-            Console.WriteLine("Please enter your name...");
-            // Assign variable 'userName' to a value entered by the user.
-            // @link https://docs.microsoft.com/en-us/dotnet/api/system.console.readline?view=netcore-3.1
-            string userName = Console.ReadLine();
-            // Show the user we collected the name.
-            Console.WriteLine("You have entered: "+userName);
-            // Instead of traditional concatenation using the addition operator...
-            // We can use string interpolation (placeholders) to embed values in
-            // a string!
-            // @link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
-            Console.WriteLine("Hey {0}, you're looking great today!", userName);
+            // Multiplication operator is "*" (multiply.)
+            Console.WriteLine( myInt * 2 ); // 84
 
-            // Let's play with strings!
-            Console.WriteLine( "makemeuppercase".ToUpper() ); // Convert to uppercase...
-            Console.WriteLine( "MAKEMELOWERCASE".ToLower() ); // Convert to lowercase...
-            // Substring takes two arguments: 1) Starting character position 2) Number of characters to collect.
-            Console.WriteLine( "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring( 3, 3 ) );
-            Console.WriteLine( "XYZ".Length ); // Get the number of characters for the string.
+            // Division operator is "/" (division.)
+            Console.WriteLine( myInt / 2 ); // 21
+
+            // Modulus operator is "%" (percent sign.)
+            Console.WriteLine( myInt % 13 ); // 3
+
+            // Addition operator is "+" (plus.)
+            // ***WAIT! This is ALSO the concatenation operator.
+            Console.WriteLine( myInt + 8 ); // 50
+
+            Console.WriteLine( "3" + "3" ); // 33
+
+            Console.WriteLine( "3" + 3 ); // 33
+            Console.WriteLine( 3 + 6 + "3" ); // 93
         }
     }
 }
