f18b33a04b84610cc1279bcd4fbed45cc799cfcf
Warren Uhrich <hello@warren.codes>
2020-06-25 15:54:15 -0600

    Add fix for 're-assignment' in our calculator!

--- Program.cs
+++ Program.cs
@@ -13,32 +13,34 @@ namespace c_sharp_intro
             string userInput;
             while ( ( userInput = Console.ReadLine() ) != "exit" ) // LOOPS until a user enters "exit" command.
             {
-                
+                int firstNum;
+                int secondNum;
+                int result;
                 // Switch statements are similar to if/else.
                 switch ( userInput ) // Here we enter the value we want to test.
                 { // Cases are us testing the above against specific values.
                     case "add":
                         Console.WriteLine( "Enter first number for addition:" );
-                        int firstNum = Int32.Parse( Console.ReadLine() );
+                        firstNum = Int32.Parse( Console.ReadLine() );
                         Console.WriteLine( "Enter second number for addition:" );
-                        int secondNum = Int32.Parse( Console.ReadLine() );
-                        int result = firstNum + secondNum;
+                        secondNum = Int32.Parse( Console.ReadLine() );
+                        result = firstNum + secondNum;
                         Console.WriteLine( "{0} + {1} = {2}", firstNum, secondNum, result );
                         break;
                     case "subtract":
                         Console.WriteLine( "Enter first number for subtraction:" );
-                        int firstNum = Int32.Parse( Console.ReadLine() );
+                        firstNum = Int32.Parse( Console.ReadLine() );
                         Console.WriteLine( "Enter second number for subtraction:" );
-                        int secondNum = Int32.Parse( Console.ReadLine() );
-                        int result = firstNum - secondNum;
+                        secondNum = Int32.Parse( Console.ReadLine() );
+                        result = firstNum - secondNum;
                         Console.WriteLine( "{0} - {1} = {2}", firstNum, secondNum, result );
                         break;
                     case "multiply":
                         Console.WriteLine( "Enter first number for multiply:" );
-                        int firstNum = Int32.Parse( Console.ReadLine() );
+                        firstNum = Int32.Parse( Console.ReadLine() );
                         Console.WriteLine( "Enter second number for multiply:" );
-                        int secondNum = Int32.Parse( Console.ReadLine() );
-                        int result = firstNum * secondNum;
+                        secondNum = Int32.Parse( Console.ReadLine() );
+                        result = firstNum * secondNum;
                         Console.WriteLine( "{0} * {1} = {2}", firstNum, secondNum, result );
                         break;
                     default:
