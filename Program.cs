7044fe9fdb7c9e9ecf2db492ab357dcd2d38f1d1
Warren Uhrich <hello@warren.codes>
2020-06-26 10:02:33 -0600

    Add 2-dimensional array example; add foreach example.

--- Program.cs
+++ Program.cs
@@ -6,49 +6,16 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            // Guide the user...
-            Console.WriteLine( "Please enter add/subtract/multiply/exit:" );
-
-            // Collect the user input...
-            string userInput;
-            while ( ( userInput = Console.ReadLine() ) != "exit" ) // LOOPS until a user enters "exit" command.
+            // 2 dimensional array!
+            string[,] topics = {{"cat","dog","frog","hedgehog"},
+                                {"zebra","giraffe","lion","elephant"},
+                                {"dolphin","octopus","shark","whale"}};
+            // We can use a foreach to iterate through the list of lists, one list at a time.
+            foreach( string topic in topics )
             {
-                int firstNum;
-                int secondNum;
-                int result;
-                // Switch statements are similar to if/else.
-                switch ( userInput ) // Here we enter the value we want to test.
-                { // Cases are us testing the above against specific values.
-                    case "add":
-                        Console.WriteLine( "Enter first number for addition:" );
-                        firstNum = Int32.Parse( Console.ReadLine() );
-                        Console.WriteLine( "Enter second number for addition:" );
-                        secondNum = Int32.Parse( Console.ReadLine() );
-                        result = firstNum + secondNum;
-                        Console.WriteLine( "{0} + {1} = {2}", firstNum, secondNum, result );
-                        break;
-                    case "subtract":
-                        Console.WriteLine( "Enter first number for subtraction:" );
-                        firstNum = Int32.Parse( Console.ReadLine() );
-                        Console.WriteLine( "Enter second number for subtraction:" );
-                        secondNum = Int32.Parse( Console.ReadLine() );
-                        result = firstNum - secondNum;
-                        Console.WriteLine( "{0} - {1} = {2}", firstNum, secondNum, result );
-                        break;
-                    case "multiply":
-                        Console.WriteLine( "Enter first number for multiply:" );
-                        firstNum = Int32.Parse( Console.ReadLine() );
-                        Console.WriteLine( "Enter second number for multiply:" );
-                        secondNum = Int32.Parse( Console.ReadLine() );
-                        result = firstNum * secondNum;
-                        Console.WriteLine( "{0} * {1} = {2}", firstNum, secondNum, result );
-                        break;
-                    default:
-                        Console.WriteLine( "Unrecognized operator/command entered." );
-                        break;
-                } // End of switch statement.
-                Console.WriteLine( "Please enter a new command: add/subtract/multiply/exit." );
-            } // End of while statement.
+                // Output the current animal!
+                Console.WriteLine( "This animal is: {0}", topic );
+            }
         }
     }
 }
