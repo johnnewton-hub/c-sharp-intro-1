10ca1516b10084c9973d5526f00c6b41ffe13b44
Warren Uhrich <hello@warren.codes>
2020-06-26 11:43:29 -0600

    Add input parse method; add try/catch statement example; add output exception (basic add/subtract calculator example.)

--- Program.cs
+++ Program.cs
@@ -16,18 +16,18 @@ namespace c_sharp_intro
                 if ( command == "add" )
                 {
                     Console.WriteLine( "Enter first number to add:" );
-                    int firstNum = int.Parse( Console.ReadLine() );
+                    int firstNum = CollectIntFromUser();
                     Console.WriteLine( "Enter second number to add:" );
-                    int secondNum = int.Parse( Console.ReadLine() );
+                    int secondNum = CollectIntFromUser();
                     result = Addition( firstNum, secondNum );
                     Console.WriteLine( "The result is: {0}", result );
                 }
                 else if ( command == "subtract" )
                 {
                     Console.WriteLine( "Enter first number to subtract:" );
-                    int firstNum = int.Parse( Console.ReadLine() );
+                    int firstNum = CollectIntFromUser();
                     Console.WriteLine( "Enter second number to subtract:" );
-                    int secondNum = int.Parse( Console.ReadLine() );
+                    int secondNum = CollectIntFromUser();
                     result = Subtraction( firstNum, secondNum );
                     Console.WriteLine( "The result is: {0}", result );
                 }
@@ -47,6 +47,27 @@ namespace c_sharp_intro
         static int Subtraction( int num1, int num2 )
         {
             return num1 - num2;
+        }
+
+        static int CollectIntFromUser()
+        {
+            int intValue = 0;
+            bool error = true;
+            while ( error == true )
+            {
+                string userValue = Console.ReadLine();
+                try // Wrap potentially-failing code in a try - this will prevent an unhandled exception (fatal error for your program.)
+                {
+                    intValue = int.Parse( userValue ); // Attempt to convert the string...
+                    error = false; // If we get here, we're good to return the int!
+                }
+                catch( Exception exception )
+                { // We use "catch" to decide what happens if the "try" has an error!
+                    Console.WriteLine( "Invalid value entered. Please enter a number." );
+                    Console.WriteLine( exception.Message ); // The exception has its own error message - helpful to know what is failing!
+                }
+            } // End of the while loop.
+            return intValue; // Ends execution of the method, and passes the value back.
         }
     }
 }
