d4f6236e25c9e73cffde3d00f7e8e21effbe7675
Warren Uhrich <hello@warren.codes>
2020-06-26 10:27:14 -0600

    Add for loop example for 2-D arrays (hint: good for Tic Tac Toe.)

--- Program.cs
+++ Program.cs
@@ -9,12 +9,21 @@ namespace c_sharp_intro
             // 2 dimensional array!
             string[,] topics = {{"cat","dog","frog","hedgehog"},
                                 {"zebra","giraffe","lion","elephant"},
-                                {"dolphin","octopus","shark","whale"}};
-            // We can use a foreach to iterate through the list of lists, one list at a time.
-            foreach( string topic in topics )
+                                {"dolphin","octopus","shark","whale"},
+                                {"ant","beetle","waterbug","mosquito"}};
+            // Loop through the rows...
+            for( int row = 0; row < topics.GetLength(0); row++ ) // row = row + 1;
             {
-                // Output the current animal!
-                Console.WriteLine( "This animal is: {0}", topic );
+                // Output current row...
+                Console.WriteLine( "Starting row #{0}", row );
+                // Loop through columns in the current row...
+                for ( int column = 0; column < topics.GetLength(1); column++ )
+                {
+                    // Output the current column...
+                    Console.WriteLine( "Starting column #{0}", column );
+                    // Output the current animal!
+                    Console.WriteLine( "This animal is: {0}", topics[row,column] );
+                }
             }
         }
     }
